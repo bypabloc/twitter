26ccf1005f61f70805e8b3c46be51472e4678bcd
Pablo Contreras <pacg1991@gmail.com>
2021-07-05 04:09:47 -0500

    profile bypabloc

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,20 @@
 <template>
     <div :class="'container theme-'+[theme]+'-200'">
         <div class="center">
-            Welcome
+            <GithubProfile width="width" user="bypabloc"></GithubProfile>
+
         </div>
     </div>
 </template>
 
 <script>
 
+import GithubProfile from 'vue-github-profile'
+
 export default {
     name: 'Welcome',
+    components: {
+        GithubProfile
+    },
 };
 </script>
